8f780767a67f96be2c57b49b9bd0768174c6d41f
nihalcshah <nihalcshah@gmail.com>
2023-10-19 00:16:29 -0400

    Mobile Functionality

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import { TextLoop } from "easy-react-text-loop";
 import { Accordion } from 'flowbite-react';
+import {isMobile} from 'react-device-detect';
 
+
+function toggleSpeedDial(event){
+  if(isMobile){
+    if(event.target.parentNode.firstChild.classList.contains("hidden")){
+      event.target.parentNode.firstChild.classList.remove("hidden")
+    }else{
+      event.target.parentNode.firstChild.classList.add("hidden")
+    }
+  }
+}
 
 function App() {
+
 
   const experienceList = [
     {
@@ -71,7 +83,7 @@ function App() {
           </div>
         </div>
 
-        <div data-dial-init class="absolute right-10 bottom-6 group">
+        <div data-dial-init class="absolute right-6 bottom-6 group">
           <div id="speed-dial-menu-default" class="flex flex-col items-center hidden mb-4 space-y-2">
             <button type="button" data-tooltip-target="tooltip-share" data-tooltip-placement="left" class="flex justify-center items-center w-[52px] h-[52px] text-gray-500 hover:text-gray-900 bg-white rounded-full border border-gray-200 dark:border-gray-600 shadow-sm dark:hover:text-white dark:text-gray-400 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:focus:ring-gray-400">
               <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
@@ -117,7 +129,7 @@ function App() {
               <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
           </div>
-          <button type="button" data-dial-toggle="speed-dial-menu-default" aria-controls="speed-dial-menu-default" aria-expanded="false" class="flex items-center justify-center text-white bg-blue-700 rounded-full w-14 h-14 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
+          <button type="button" onClick={toggleSpeedDial} data-dial-toggle="speed-dial-menu-default" aria-controls="speed-dial-menu-default" aria-expanded="false" class="flex items-center justify-center text-white bg-blue-700 rounded-full w-14 h-14 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
             <svg class="w-5 h-5 transition-transform group-hover:rotate-45" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
             </svg>
